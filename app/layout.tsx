2a8653a05a07aa911e492ea54b1fb0df7ad65548
mvttheos <mvttheos@gmail.com>
2024-05-31 22:23:35 +0000

    removing default assets

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from '@next/font/google';
 import "./globals.css";
 
-
 import CustomCursorShadow  from './components/customCursor/CustomCursor'
 
 const inter = Inter({
