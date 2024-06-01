ffa450c301abcc80ecf43f24d4316f57d5bd5d9b
mvttheos <mvttheos@gmail.com>
2024-06-01 00:08:49 +0000

    fixing link in Experience

--- app/components/experience/ExperienceCard.tsx
+++ app/components/experience/ExperienceCard.tsx
@@ -30,9 +30,8 @@ export const ExperienceCard = ({ startDate, endDate, name, description, stack, i
                 <div className="flex">
                     <Link href={""}>
                         <h3 className="group-hover:text-[#7167ff] font-medium leading-snug text-slate-200 inline-block">{name}</h3>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="group-hover:translate-x-1 group-hover:-translate-y-1 transition-transform duration-200 ml-2 max-h-5 max-w-5 h-5 w-5" aria-hidden="true"><path fill-rule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clip-rule="evenodd"></path></svg>
                     </Link>
-
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="group-hover:translate-x-1 group-hover:-translate-y-1 transition-transform duration-200 ml-2 max-h-5 max-w-5 h-5 w-5" aria-hidden="true"><path fill-rule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clip-rule="evenodd"></path></svg>
                 </div>
                 <p className="mt-2 text-sm leading-normal text-slate-400">{description}</p>
                 <ExternalLinks items={items} />
